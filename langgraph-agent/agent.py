d43819c19e2d4021250b8d021b5898b4bb331f41
zhen hu <1027551108@qq.com>
2026-09-08 19:15:23 +0800

    Add LangGraph long-term memory

--- langgraph-agent/agent.py
+++ langgraph-agent/agent.py
@@ -10,11 +10,12 @@ from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.types import Command
 from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.store.sqlite import SqliteStore
 import sqlite3
 from state import AgentState
 from model import LocalQwenChatModel
-from nodes import ModelNode, ToolArgsCompletionNode, ToolReviewNode
-from tools import read_resume, search_project_evidence
+from nodes import ModelNode, ToolArgsCompletionNode, ToolReviewNode, MemoryWriteNode
+from tools import read_resume, search_project_evidence, update_user_profile
 from langchain_core.messages import SystemMessage, HumanMessage
 from routers import router_after_tool_review
 import json
@@ -35,50 +36,114 @@ model_node = ModelNode(model_with_tools)
 tool_args_completion_node = ToolArgsCompletionNode(tools, tool_hitl_policy)
 tool_review_node = ToolReviewNode(tool_hitl_policy)
 tool_node = ToolNode(tools)
+memory_write_node = MemoryWriteNode(model, update_user_profile)
 
 graph_builder = StateGraph(AgentState)
 graph_builder.add_node("model", model_node)
 graph_builder.add_node("tools", tool_node)
 graph_builder.add_node("tool_args_completion", tool_args_completion_node)
 graph_builder.add_node("tool_review", tool_review_node)
+graph_builder.add_node("memory_write", memory_write_node)
 graph_builder.add_edge(START, "model")
-graph_builder.add_conditional_edges("model", tools_condition, {"tools":"tool_args_completion", "__end__":END})
+graph_builder.add_conditional_edges("model", tools_condition, {"tools":"tool_args_completion", "__end__":"memory_write"})
 graph_builder.add_conditional_edges("tool_args_completion",tool_args_completion_node.router_after_args_completion)
 #tools_condition就是一个很简单的路由，有tool_calls且>=1,返回tools，否则返回__end__
 graph_builder.add_conditional_edges("tool_review", router_after_tool_review)
 graph_builder.add_edge("tools", "model")
+graph_builder.add_edge("memory_write", END)
+checkpoint_connection = sqlite3.connect("checkpoints.sqlite", check_same_thread=False)
+checkpointer = SqliteSaver(checkpoint_connection)
+memory_connection = sqlite3.connect("memory.sqlite", check_same_thread=False, isolation_level=None)
+memory_store = SqliteStore(memory_connection)
+memory_store.setup()#也可不写，当然checkpointer.setup()也可以写。
+compiled_state_graph = graph_builder.compile(checkpointer=checkpointer, store=memory_store)
 
-connection = sqlite3.connect("checkpoints.sqlite", check_same_thread=False)
-checkpointer = SqliteSaver(connection)
-compiled_state_graph = graph_builder.compile(checkpointer=checkpointer)
 
+# initial_state = {
+#                     "messages" : [
+#                                     SystemMessage(content="你是一个求职助手。需要读取简历时必须调用工具，只能依据工具返回的内容回答。"),
+#                                     HumanMessage(content="请读取简历，并告诉我求职方向。")
+#                                  ],
+#                     "model_call_count" : 0
+#                 }
 
-initial_state = {
-                    "messages" : [
-                                    SystemMessage(content="你是一个求职助手。需要读取简历时必须调用工具，只能依据工具返回的内容回答。"),
-                                    HumanMessage(content="请读取简历，并告诉我求职方向。")
-                                 ],
-                    "model_call_count" : 0
-                }
+# if __name__ == "__main__":
+#     config = {
+#         "configurable": {
+#             "thread_id": "hitl_reject_test_112"
+#         }
+#     }
+#     context = {"user_id" : "user_A"}
+#     response = compiled_state_graph.invoke(input=initial_state, config=config, context=context)
+#     while "__interrupt__" in response:
+#         interrupt_data = response["__interrupt__"][0]
+#         print("interrupt_request:", interrupt_data.value)
+#         print("................")
+        
+#         resume_text = input("请输入resume_value(json): ")
+#         resume_response = json.loads(resume_text)
+        
+#         response = compiled_state_graph.invoke(
+#             input=Command(resume=resume_response),
+#             config=config,
+#             context=context
+#         )
+
+#     print("final_response:", response)
+system_message = SystemMessage(
+    content="你是一个求职助手，只能依据已知信息回答。"
+)
 
 if __name__ == "__main__":
-    config = {
-        "configurable": {
-            "thread_id": "hitl_reject_test_112"
-        }
-    }
-    response = compiled_state_graph.invoke(input=initial_state, config=config)
-    while "__interrupt__" in response:
-        interrupt_data = response["__interrupt__"][0]
-        print("interrupt_request:", interrupt_data.value)
-        print("................")
-        
-        resume_text = input("请输入resume_value(json): ")
-        resume_response = json.loads(resume_text)
-        
-        response = compiled_state_graph.invoke(
-            input=Command(resume=resume_response),
-            config=config
-        )
+    context = {"user_id": "user_A"}
 
-    print("final_response:", response)
+    thread_A_state = {
+        "messages": [
+            system_message,
+            HumanMessage(
+                content=(
+                    "请记住：我长期希望在深圳找Agent工程师工作，"
+                    "我有9年深度学习经验。"
+                )
+            ),
+        ],
+        "model_call_count": 0,
+    }
+
+    compiled_state_graph.invoke(
+        input=thread_A_state,
+        config={
+            "configurable": {
+                "thread_id": "memory_test_thread_A_gast"
+            }
+        },
+        context=context,
+    )
+
+    profile_item = memory_store.get(
+        ("user_A", "memories"),
+        "profile",
+    )
+    print("memory:", profile_item.value)
+
+    thread_B_state = {
+        "messages": [
+            system_message,
+            HumanMessage(
+                content="我希望在哪个城市找什么工作？我有多少年经验？"
+            ),
+        ],
+        "model_call_count": 0,
+    }
+
+    response = compiled_state_graph.invoke(
+        input=thread_B_state,
+        config={
+            "configurable": {
+                "thread_id": "memory_test_thread_B_gast"
+            }
+        },
+        context=context,
+    )
+
+    print("assistant:", response["messages"][-1].content)
--- langgraph-agent/nodes.py
+++ langgraph-agent/nodes.py
@@ -8,14 +8,25 @@ Created on Mon Sep  7 10:00:38 2026
 
 from langgraph.types import interrupt
 from copy import deepcopy
-from langchain_core.messages import ToolMessage
+from langchain_core.messages import ToolMessage, SystemMessage, HumanMessage
+import json
 
 class ModelNode:
     def __init__(self, model):
         self.model = model
         
-    def __call__(self, state):
-        ai_message = self.model.invoke(state["messages"])
+    def __call__(self, state, runtime):
+        messages = deepcopy(state["messages"])
+        user_id = runtime.context["user_id"]
+        namespace = (user_id, "memories")
+        profile_item = runtime.store.get(namespace, "profile")
+        if profile_item is not None:
+            profile = profile_item.value
+            profile_content = json.dumps(profile, ensure_ascii=False)
+            messages[0].content += (
+                "\n\n用户长期记忆：\n{}".format(profile_content)
+            )
+        ai_message = self.model.invoke(messages)
         update_state = {
                 "messages" : [ai_message],
                 "model_call_count" : 1
@@ -138,4 +149,77 @@ class ToolReviewNode:
         updated_messages = [updated_ai_message]
         updated_messages.extend(rejected_tool_messages)
         return {"messages":updated_messages}
+   
+class MemoryWriteNode:
+    def __init__(self, model, memory_tool):
+        self.memory_tool = memory_tool
+        self.model = model.bind_tools([memory_tool])
+        self.system_message = SystemMessage(
+                       content=(
+                            "你是长期记忆提取器。"
+                            "只分析latest_user_message中用户明确表达的长期求职信息。"
+                            "existing_profile只用于理解用户对已有信息的修改。"
+                            "临时查询、一次性要求、疑问和模型推测不能保存。"
+                            "存在需要新增、修改或明确删除的长期信息时，"
+                            "调用update_user_profile工具。"
+                            "没有需要更新的信息时，不调用任何工具。"
+                       )
+        )
         
+    def __call__(self, state, runtime):
+        latest_user_message = None
+        for message in reversed(state["messages"]):
+            if isinstance(message, HumanMessage):
+                latest_user_message = message
+                break
+            
+        if latest_user_message is None:
+            return {}
+        
+        user_id = runtime.context["user_id"]
+        namespace = (user_id, "memories")
+        key = "profile"
+        profile_item = runtime.store.get(namespace, key)
+        if profile_item is None:
+            existing_profile = {}
+        else:
+            existing_profile = profile_item.value
+        
+        user_message = HumanMessage(
+                        content = json.dumps(
+                            {
+                                "existing_profile" : existing_profile,
+                                "latest_user_message" : latest_user_message.content
+                            },
+                            ensure_ascii=False
+                        )
+        )
+        memory_extraction_messages = [self.system_message, user_message]
+        
+        response = self.model.invoke(memory_extraction_messages)
+        
+        tool_calls = response.tool_calls
+        if not tool_calls:
+            return {"model_call_count":1}
+        
+        if len(tool_calls) != 1:
+            raise ValueError("长期记忆提取模型最多只能返回一个ToolCall")
+        
+        tool_call = tool_calls[0]
+        if tool_call["name"] != self.memory_tool.name:
+            raise ValueError("长期记忆提取模型只能调用:{}".format(self.memory_tool.name))
+        profile_changes = self.memory_tool.invoke(tool_call["args"])
+        profile_updates = profile_changes["updates"]
+        fields_to_delete = profile_changes["fields_to_delete"]
+        if not profile_updates and not fields_to_delete:
+            return {"model_call_count":1}
+        merged_profile = {**existing_profile, **profile_updates}
+        if fields_to_delete:
+            merged_profile = {k:v for k,v in merged_profile.items() if k not in fields_to_delete}
+        
+        if merged_profile:
+            runtime.store.put(namespace, key, merged_profile)
+        else:
+            runtime.store.delete(namespace, key)
+        
+        return {"model_call_count" : 1}
